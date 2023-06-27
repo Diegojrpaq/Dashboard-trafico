4660adebd00b95183a9464dff1244e7cec775033
legolasblack <legolas.black.ds@gmail.com>
2023-06-27 12:29:12 -0600

    lectura del sidebar atravez del json dibujando los destinos solo falta crear un estado para manipular la renderizacion del componente

--- dashboard-front/src/App.jsx
+++ dashboard-front/src/App.jsx
@@ -1,128 +1,137 @@
 /* import Swal from 'sweetalert2' */
 import { createContext, useEffect, useState } from 'react';
-import Graficalinea from './Componentes/Graficalinea';
-import GraphChart from './Componentes/GraphChart';
+/* import Graficalinea from './Componentes/Graficalinea';
+import GraphChart from './Componentes/GraphChart'; */
 import Navbar from './Componentes/Navbar';
 import SideBar from './Componentes/SideBar';
-import Data from './Data/Data.json';
+import Data from './Data/Data2.json';
 /* import Swal from 'sweetalert2' */
 
 
-const contextDataLogistic = createContext()
+export const dataLogisticContext = createContext()
 
 
 
 function App() {
 
- /*  const urlApiJrTrafico = 'https://pokeapi.co/api/v2/pokemon/snorlax'; */
-  const [dataTraficoState, setDataTraficoState] = useState(null);
+  
+  const [dataLogisticState, setDataLogistic] = useState(null);
+  
 
-/*   useEffect(() => {
- */
+  /*  const urlApiJrTrafico = 'https://pokeapi.co/api/v2/pokemon/snorlax'; */
 
-    /*  fetch(urlApiJrTrafico).then((resp) => {
-       return resp.json();
-     }).then((data) => {
-       setDataTraficoState(data)
-       if (data) {
-         Swal.fire(
-           'Good job!',
-           'Se recibio la informacion correctamente',
-           'success'
-         )
-       }
-     }).catch(
-       () => console.log('Error al cargar el rastreo ')
-     ) */
+  /*   useEffect(() => {
+   */
 
-/*     if(Data){
-      setDataTraficoState(Data)//esta linea es el harcodeo de cargar los datos hacia el estado lo correcto es cargarlo de la api
-    }else{
-      console.log('cargando...')
-    }
-
-    console.log(dataTraficoState)
-  }, [dataTraficoState]); */
+  /*  fetch(urlApiJrTrafico).then((resp) => {
+     return resp.json();
+   }).then((data) => {
+     setDataTraficoState(data)
+     if (data) {
+       Swal.fire(
+         'Good job!',
+         'Se recibio la informacion correctamente',
+         'success'
+       )
+     }
+   }).catch(
+     () => console.log('Error al cargar el rastreo ')
+   ) */
 
 
-
-  return (
-    <>
-      <contextDataLogistic.Provider value= {dataTraficoState}>
-        {/* navbar bootstrap */}
-        <Navbar></Navbar>
-        {/* navbar bootstrap */}
+    useEffect(() => {
+      
+     setDataLogistic(Data)
+    }, [Data]);
 
 
-        {/* contenedor principal */}
 
-        <div className="container-fluid g-0">
-          <div className="row">
-            <div className="col-2">
-              <SideBar></SideBar>
-            </div>
-            <div className="col-10">
-              <div className="row"><h1>tabla</h1></div>
+    if(dataLogisticState!=null){
+      return (
+        <>
+          <dataLogisticContext.Provider value={dataLogisticState}>
+
+            {/* navbar bootstrap */}
+            <Navbar></Navbar>
+            {/* navbar bootstrap */} 
+            {/* contenedor principal */}
+            <div className="container-fluid g-0">
               <div className="row">
-                <div className="col-xl-6 col-12">
-                  <div className="contenedordechart">
-                   {/*  <Graficalinea></Graficalinea> */}
+                <div className="col-2">
+                  <SideBar></SideBar>
+                </div>
+                <div className="col-10">
+                  <div className="row"><h1>tabla</h1></div>
+                  <div className="row">
+                    <div className="col-xl-6 col-12">
+                      <div className="contenedordechart">
+                        {/*  <Graficalinea></Graficalinea> */}
+                      </div>
+                    </div>
+                    <div className="col-xl-6 col-12">
+                      <div className="contenedordechart">
+                        {/* <GraphChart></GraphChart> */}
+                      </div>
+                    </div>
+                    <div className="col-4"></div>
                   </div>
                 </div>
-                <div className="col-xl-6 col-12">
-                  <div className="contenedordechart">
-                    {/* <GraphChart></GraphChart> */}
-                  </div>
-                </div>
-                <div className="col-4"></div>
               </div>
             </div>
+    
+            {/* contenedor principal */}
+    
+    
+           
+    
+          </dataLogisticContext.Provider>
+    
+        </>
+    
+      );//fin del return 
+    }else{console.log("cargando los datos de la api")
+    }
+  
+}
+
+
+function apartadocodigo(){
+ return(
+  <div className="container">
+  <div className="row">
+    <div className="col-2">
+
+    </div>
+    <div className="col-10">
+      <div>
+        <h1>graficas charts</h1>
+        <div>
+          <p className='m-2'><b>ejemplo #1 :</b> Grafico de lineas basicas</p>
+          <div className="bg-ligth mx-auto px-2 border border-2 border-primary container-chart">
+            {/*  <Graficalinea></Graficalinea> */}
           </div>
         </div>
-
-        {/* contenedor principal */}
-
-
-        <div className="container">
-          <div className="row">
-            <div className="col-2">
-
-            </div>
-            <div className="col-10">
-              <div>
-                <h1>graficas charts</h1>
-                <div>
-                  <p className='m-2'><b>ejemplo #1 :</b> Grafico de lineas basicas</p>
-                  <div className="bg-ligth mx-auto px-2 border border-2 border-primary container-chart">
-                   {/*  <Graficalinea></Graficalinea> */}
-                  </div>
-                </div>
-                <hr />
-                <div>
-                  <p className='m-2'><b>ejemplo #1 :</b> Grafico de barras basicas</p>
-                  <div className="bg-ligth mx-auto px-2 border border-2 border-primary  container-chart">
-                  {/*   <GraphChart></GraphChart> */}
-                  </div>
-                </div>
-                <hr />
-                <div>
-                  <p className='m-2'><b>ejemplo #1 :</b> Grafico circular basicas</p>
-                  <div className="bg-ligth mx-auto px-2 border border-2 border-primary container-chart">
-                  </div>
-                </div>
-                <hr />
-              </div>
-
-
-            </div>
+        <hr />
+        <div>
+          <p className='m-2'><b>ejemplo #1 :</b> Grafico de barras basicas</p>
+          <div className="bg-ligth mx-auto px-2 border border-2 border-primary  container-chart">
+            {/*   <GraphChart></GraphChart> */}
           </div>
         </div>
+        <hr />
+        <div>
+          <p className='m-2'><b>ejemplo #1 :</b> Grafico circular basicas</p>
+          <div className="bg-ligth mx-auto px-2 border border-2 border-primary container-chart">
+          </div>
+        </div>
+        <hr />
+      </div>
 
-      </contextDataLogistic.Provider>
 
-    </>
-
-  );
+    </div>
+  </div>
+</div>
+ );
 }
 
 export default App;
--- dashboard-front/src/Componentes/SideBar.jsx
+++ dashboard-front/src/Componentes/SideBar.jsx
@@ -1,170 +1,72 @@
-import React, { useState } from 'react';
+import React, { useContext} from 'react';
+import { dataLogisticContext } from '../App';
 
-const SideBar = () => {
+export default function SideBar() {
 
-  const [collapseOneOpen, setCollapseOneOpen] = useState(true);
-  const [collapseTwoOpen, setCollapseTwoOpen] = useState(false);
+  const Data = useContext(dataLogisticContext);
 
-  const handleCollapseOneToggle = () => {
-    setCollapseOneOpen(!collapseOneOpen);
-  };
+  console.log('cargando dattos')
+  
 
-  const handleCollapseTwoToggle = () => {
-    setCollapseTwoOpen(!collapseTwoOpen);
-  };
 
-  return (
-    <div className="accordion" id="accordionExample">
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button
-            className="accordion-button"
-            type="button"
-            onClick={handleCollapseOneToggle}
-            aria-expanded={collapseOneOpen}
-            aria-controls="collapseOne"
-          >
-            Destinos
-          </button>
-        </h2>
-        <div
-          id="collapseOne"
-          className={`accordion-collapse collapse ${collapseOneOpen ? 'show' : ''}`}
-        >
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Sucursal"
-              aria-label="Example text with button addon"
-              aria-describedby="button-addon1"
-            />
-            <button className="btn btn-outline-primary" type="button" id="button-addon1">
-              Search
-            </button>
+
+  if (Data === undefined) {
+    console.log('ya recibimos' + Data)
+    return (
+      <>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {
+          console.log('nunca llega ' + Data)
+        }
+        <h1>SideBar</h1>
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-controls="collapseOne">
+                Destino One
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+
+
+
+              {
+                Data.Destinos.map((x, i) => {
+                  return (
+                    <div className="accordion-body" id={i}>
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label className="form-check-label" for="flexCheckDefault">
+                          {x.destino}
+                        </label>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+
+
+
+            </div>
           </div>
-          <div className="accordion-body">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Gonzalez gallo
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Lazaro Cardenas
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Perisur
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Queretaro
-              </label>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Accordion Item #3
+              </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingTwo">
-          <button
-            className="accordion-button collapsed"
-            type="button"
-            onClick={handleCollapseTwoToggle}
-            aria-expanded={collapseTwoOpen}
-            aria-controls="collapseTwo"
-          >
-            Destinos
-          </button>
-        </h2>
-        <div
-          id="collapseTwo"
-          className={`accordion-collapse collapse ${collapseTwoOpen ? 'show' : ''}`}
-        >
-          <div className="accordion-body">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Gonzalez gallo
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Lazaro Cardenas
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Perisur
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="flexCheckChecked"
-                checked
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Queretaro
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
 };
-
-export default SideBar;
