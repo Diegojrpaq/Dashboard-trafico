0abf629aafe646e0fe2890f1db3baa6d856d88b1
legolasblack <legolas.black.ds@gmail.com>
2023-09-11 09:14:28 -0600

    agregamos libreria react-bootstrap

--- dashboard-front/src/Componentes/SideBar2.jsx
+++ dashboard-front/src/Componentes/SideBar2.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import '../Css/Sidebar2.css'
+import { Accordion, Card } from 'react-bootstrap';
 import { dataLogisticContext } from '../App';
 import Logo from '../assets/img/logo.png'
 
@@ -13,8 +14,33 @@ export default function SideBar2() {
 
         <img src={Logo} alt="" className='img-fluid mb-4' />
         <p>Contenido del sidebar</p>
-
-           
+    <Accordion defaultActiveKey="0"  >
+      <Accordion.Item eventKey="0">
+        <Accordion.Header className='Accordion-custom' >Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  
         <ul>
 
         </ul>
