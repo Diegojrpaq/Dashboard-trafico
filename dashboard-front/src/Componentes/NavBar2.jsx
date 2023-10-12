d577922fe3195924671bb8377a629396110116e0
Cris <cristopher.26hp@gmail.com>
2023-10-12 12:30:26 -0600

    Se modifico la funcion CalcularAncho

--- dashboard-front/src/Componentes/NavBar2.jsx
+++ dashboard-front/src/Componentes/NavBar2.jsx
@@ -27,18 +27,17 @@ export default function Navbar() {
                     </button>
                     <a className="navbar-brand" href="#">{tituloNav}</a>
                 </div>
-
-                <div className='col-7 d-none d-lg-flex flex-column justify-content-center align-items-end'>
+                <div className="col-3 form-check form-switch d-none d-lg-flex justify-content-end align-items-center fs-4">
+                    <input onClick={handleClic} className="form-check-input align-self-center" type="checkbox" id="flexSwitchCheckDefault" />
+                    <span className='fs-6 align-self-center text-light ps-1 pt-1 '>{btnSwitch ? "ON" : "OFF"}</span>
+                </div>
+                <div className='col-3 d-none d-lg-flex flex-column justify-content-center align-items-end'>
                     <div className='row'>
-                        <div className="col-1 form-check form-switch d-flex justify-content-center align-items-center fs-4 me-5">
-                            <input onClick={handleClic} className="form-check-input align-self-center" type="checkbox" id="flexSwitchCheckDefault" />
-                            <span className='fs-6 align-self-center text-light ps-2 pt-1 '>{btnSwitch ? "ON" : "OFF"}</span>
-                        </div>
                         <div className='col-1 d-flex justify-content-center align-items-center'>
                             <i className="bi bi-person-circle fs-2 text-light"></i>
                         </div>
                         <div className='col d-none d-lg-flex flex-column justify-content-center align-items-start'>
-                            <span className='spanUser' style={{"color":"white"}}>
+                            <span className='spanUser' style={{ "color": "white" }}>
                                 {userData ? userData.nombre : ""}
                             </span>
                             <span className='spanPuesto'>
