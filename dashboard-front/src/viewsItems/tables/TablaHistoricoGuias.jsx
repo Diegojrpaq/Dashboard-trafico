d99df484eefc27616e2066134d6704bb34b69d64
Cris <cristopher.26hp@gmail.com>
2023-11-27 14:10:50 -0600

    commit para hacer merge

--- dashboard-front/src/viewsItems/tables/TablaHistoricoGuias.jsx
+++ dashboard-front/src/viewsItems/tables/TablaHistoricoGuias.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function TablaHistoricoGuias(props) {
     console.log(props.guias, "Guias")
   return (
-    <div>TablaHistoricoGuias</div>
+    <div>Tabla Historico Guias</div>
   )
 }
